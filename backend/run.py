e553bfc1eab9c1f547a2809f4f1d2366a4ce62ea
thaninbew <kongkiatsophon.t@northeastern.edu>
2025-02-08 00:17:32 -0500

    runpy

--- backend/run.py
+++ backend/run.py
@@ -3,8 +3,8 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
-        port=3000,
+        host="127.0.0.1",
+        port=5000,
         reload=True,
         log_level="info"
     ) 
