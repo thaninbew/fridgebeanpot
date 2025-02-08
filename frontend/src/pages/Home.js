305ffef858aa004b0950ef5e23f9c8ae72736db2
jolinhuangg <jolinhuang67@gmail.com>
2025-02-08 13:24:56 -0500

    finished home
    needs to implement backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,39 +1,47 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen relative bg-[#f8f5ec] overflow-hidden flex flex-col items-center">
-      <div className="absolute top-[20vh] left-50% text-center text-black text-[65px] font-semibold">
-        fridge.
-      </div>
+    <div className="w-screen h-screen relative bg-[#f8f5ec] overflow-hidden">
+      {/* Centered container with a fixed width */}
+      <div
+        className="absolute left-1/2 transform -translate-x-1/2"
+        style={{ top: "17vh", width: "60vw" }}
+      >
+        <div className="flex flex-col items-start">
+          <h1 className="text-black text-[65px] font-semibold mb-5">fridge.</h1>
 
-      <div className="absolute left-50%" style={{ top: "55vh" }}>
-        <div className="flex flex-col">
-          <div className="text-left text-black font-normal text-[30px]">
-            Hungry?
+          <div className="mt-[35vh]">
+            <div className="text-black font-normal text-[30px]">Hungry?</div>
+            <div className="text-black text-[18px] font-light">
+              Find local restaurants near you.
+            </div>
           </div>
-          <div className="text-center text-black text-[17px] font-light">
-            Find local restaurants near you.
+
+          <div className="mt-3 w-full">
+            <button
+              type="button"
+              className="w-full h-[55px] bg-[#ffdc90] rounded-[22.20px] shadow-[0px_3px_0px_0px_rgba(0,0,0,1)]
+                     active:shadow-[inset_0px_1px_0px_rgba(0,0,0,1)] border border-black
+                     flex justify-center items-center transition-shadow duration-150 transform active:translate-y-1"
+            >
+              <span className="text-black font-normal text-[20px]">Log In</span>
+            </button>
+
+            <button
+              type="button"
+              className="mt-3 w-full h-[55px] bg-[#ffdc90] rounded-[22.20px] shadow-[0px_3px_0px_0px_rgba(0,0,0,1)]
+                     active:shadow-[inset_0px_1px_0px_rgba(0,0,0,1)] border border-black
+                     flex justify-center items-center transition-shadow duration-150 transform active:translate-y-1"
+            >
+              <span className="text-black font-normal text-[20px]">
+                Sign up
+              </span>
+            </button>
           </div>
         </div>
       </div>
 
-      <div className="w-[100vw] absolute left-[97px] top-[65vh]">
-        <button
-          type="button"
-          className="w-[50vw] h-[50px] bg-[#ffdc90] rounded-[22.20px] shadow-[0px_3px_0px_0px_rgba(0,0,0,1)] 
-               active:shadow-[inset_0px_1px_0px_rgba(0,0,0,1)] border border-black 
-               flex justify-center items-center transition-shadow duration-150 transform active:translate-y-1"
-        >
-          <span className="text-black font-normal text-[20px]">Log In</span>
-        </button>
-
-        <button
-          type="button"
-          className="mt-3 w-[50vw] h-[50px] bg-[#ffdc90] rounded-[22.20px] shadow-[0px_3px_0px_0px_rgba(0,0,0,1)] 
-               active:shadow-[inset_0px_1px_0px_rgba(0,0,0,1)] border border-black 
-               flex justify-center items-center transition-shadow duration-150 transform active:translate-y-1"
-        >
-          <span className="text-black font-normal text-[20px]">Sign up</span>
-        </button>
+      <div className="flex items-center justify-center mt-[19vh]">
+        <img src="/bean-fridge.svg" alt="Logo" className="w-[50vw] h-[50vh]" />
       </div>
     </div>
   );
