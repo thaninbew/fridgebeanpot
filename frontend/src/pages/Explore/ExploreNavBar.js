1d1030a39922159ac99015280ecf8192073621ac
jolinhuangg <jolinhuang67@gmail.com>
2025-02-08 16:44:40 -0500

    aligned navbar
    put navbar inside explorenavbar for temporary fix on alignment issues

--- frontend/src/pages/Explore/ExploreNavBar.js
+++ frontend/src/pages/Explore/ExploreNavBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Navbar from '../../components/Navbar';
+import Navbar from "../../components/Navbar";
 
 export default function ExploreNavBar() {
   const initialTab = window.location.pathname === '/explore' ? 'explore' : 'map';
@@ -40,6 +40,7 @@ export default function ExploreNavBar() {
           </button>
         </div>
       </div>
+      <Navbar />
     </div>
   );
 }
--- frontend/src/pages/Explore/Map.js
+++ frontend/src/pages/Explore/Map.js
@@ -1,11 +1,10 @@
 import ExploreNavBar from "./ExploreNavBar";
-import Navbar from "../../components/Navbar";
+
 
 export default function Map() {
   return (
     <div>
       <ExploreNavBar />
-      <Navbar />
     </div>
   );
 }
