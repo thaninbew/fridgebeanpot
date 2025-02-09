438a508b061815d19480f3a17715008d96142083
jolinhuangg <jolinhuang67@gmail.com>
2025-02-09 04:22:49 -0500

    Update RecComponent.js

--- frontend/src/pages/Explore/RecComponent.js
+++ frontend/src/pages/Explore/RecComponent.js
@@ -3,7 +3,11 @@ import { FaStar } from "react-icons/fa";
 export default function RecComponent({rating, restaurant, cuisine, image}) {
     
 return (
+<<<<<<< Updated upstream
     <div className="">
+=======
+        <div className="">
+>>>>>>> Stashed changes
         <div className="w-[50vw] h-[52vw] bg-[#f7f6f4] rounded-[21px] shadow-[0px_2px_0px_0px_rgba(0,0,0,1.00)] border border-black flex justify-center">
             <div className="flex flex-col w-[100%]">
                 <img className="w-[90%] h-[65%] rounded-2xl border border-black ml-auto mr-auto" src={"/freaky-bean.svg"} />
