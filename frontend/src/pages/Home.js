c1c4a2047193dad772a90fbeeb2d8e72929beecd
jolinhuangg <jolinhuang67@gmail.com>
2025-02-08 19:12:30 -0500

    HOMEPAGE FIXXX

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -17,35 +17,37 @@ export default function Home() {
             </div>
           </div>
 
-          <div className=" mt-4 w-full">
-            <Link to="/login">
-              <button
-                type="button"
-                className="w-full h-[55px] bg-[#ffdc90] rounded-[22.20px] shadow-[0px_3px_0px_0px_rgba(0,0,0,1)]
-                     active:shadow-[inset_0px_1px_0px_rgba(0,0,0,1)] border border-black
-                     flex justify-center items-center transition-shadow duration-150 transform active:translate-y-1"
-              >
-                <span className="text-black font-normal text-[20px]">
-                  Log In
-                </span>
-              </button>
+          <div className=" mt-4 w-full z-10">
+              <Link to="/login">
+                <button
+                  type="button"
+                  className="w-full h-[55px] bg-[#ffdc90] rounded-[22.20px] shadow-[0px_3px_0px_0px_rgba(0,0,0,1)]
+                       active:shadow-[inset_0px_1px_0px_rgba(0,0,0,1)] border border-black
+                       flex justify-center items-center transition-shadow duration-150 transform active:translate-y-1"
+                >
+                  <span className="text-black font-normal text-[20px]">
+                    Log In
+                  </span>
+                </button>
+              </Link>
 
-              <button
-                type="button"
-                className="mt-3 w-full h-[55px] bg-[#ffdc90] rounded-[22.20px] shadow-[0px_3px_0px_0px_rgba(0,0,0,1)]
-                     active:shadow-[inset_0px_1px_0px_rgba(0,0,0,1)] border border-black
-                     flex justify-center items-center transition-shadow duration-150 transform active:translate-y-1"
-              >
-                <span className="text-black font-normal text-[20px]">
-                  Sign up
-                </span>
-              </button>
-            </Link>
+              <Link to="/signup">
+                <button
+                  type="button"
+                  className="mt-3 w-full h-[55px] bg-[#ffdc90] rounded-[22.20px] shadow-[0px_3px_0px_0px_rgba(0,0,0,1)]
+                       active:shadow-[inset_0px_1px_0px_rgba(0,0,0,1)] border border-black
+                       flex justify-center items-center transition-shadow duration-150 transform active:translate-y-1"
+                >
+                  <span className="text-black font-normal text-[20px]">
+                    Sign up
+                  </span>
+                </button>
+              </Link>
           </div>
         </div>
       </div>
 
-      <div className="w-[100vw] h-[82vh] flex flex-col items-center justify-center z-10">
+      <div className="w-[100vw] h-[82vh] flex flex-col items-center justify-center z-5">
         <img src="/bean-fridge.svg" alt="Logo" className="w-[50vw] h-[50vh]" />
       </div>
     </div>
