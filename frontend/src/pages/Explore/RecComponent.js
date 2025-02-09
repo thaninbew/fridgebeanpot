d021fbf032e8d22fd9acf9e8e507b0ee69c916de
jolinhuangg <jolinhuang67@gmail.com>
2025-02-08 23:06:10 -0500

    reccomponent

---
+++ frontend/src/pages/Explore/RecComponent.js
@@ -0,0 +1,11 @@
+export default function RecComponent() {
+return (
+    <div className="flex flex-col justify-center items-center w-[167px] h-[167px] relative">
+<div className="w-[167px] h-[167px] left-0 top-0 absolute bg-[#f7f6f4] rounded-[21px] shadow-[0px_2px_0px_0px_rgba(0,0,0,1.00)] border border-black" />
+<img className="w-[149px] h-28 left-[9px] top-[8px] absolute rounded-tl-2xl rounded-tr-2xl rounded-bl-[10px] rounded-br-[10px] border border-black" src="https://via.placeholder.com/149x112" />
+<div className="left-[136px] top-[142px] absolute text-black text-[10px] font-bold">3.8<br/></div>
+<div className="left-[16px] top-[122px] absolute text-black text-[15px] font-bold">Saxby’s</div>
+<div className="left-[16px] top-[141px] absolute text-black text-xs font-normal">American</div>
+</div>
+);
+}
