b996cc024c07e7d51e84934b2851d1c7ebe937b8
olivial215 <li.ol@northeastern.edu>
2025-02-09 01:28:45 -0500

    finished explore page

--- frontend/src/pages/Explore/ExploreNavBar.js
+++ frontend/src/pages/Explore/ExploreNavBar.js
@@ -13,7 +13,7 @@ export default function ExploreNavBar() {
   };
 
   return (
-    <div className="flex flex-col w-screen h-screen items-center">
+    <div className="flex flex-col w-screen items-center">
       <div className="w-[80vw] h-[9vh] relative bg-[#f7f6f4] rounded-[50px] border-2 border-black mt-10 mb-10">
         <div
           className={`absolute top-0 bottom-0 w-1/2 bg-[#84a59d] rounded-[50px] transition-transform duration-300 ${
--- frontend/src/pages/Explore/RecComponent.js
+++ frontend/src/pages/Explore/RecComponent.js
@@ -1,11 +1,35 @@
-export default function RecComponent() {
+import { FaStar } from "react-icons/fa";
+
+export default function RecComponent({rating, restaurant, cuisine, image}) {
+    
 return (
-    <div className="flex flex-col justify-center items-center w-[167px] h-[167px] relative">
-<div className="w-[167px] h-[167px] left-0 top-0 absolute bg-[#f7f6f4] rounded-[21px] shadow-[0px_2px_0px_0px_rgba(0,0,0,1.00)] border border-black" />
-<img className="w-[149px] h-28 left-[9px] top-[8px] absolute rounded-tl-2xl rounded-tr-2xl rounded-bl-[10px] rounded-br-[10px] border border-black" src="https://via.placeholder.com/149x112" />
-<div className="left-[136px] top-[142px] absolute text-black text-[10px] font-bold">3.8<br/></div>
-<div className="left-[16px] top-[122px] absolute text-black text-[15px] font-bold">Saxby’s</div>
-<div className="left-[16px] top-[141px] absolute text-black text-xs font-normal">American</div>
-</div>
-);
+        <div className="pl-[15px]">
+        <div className="w-[50vw] h-[52vw] bg-[#f7f6f4] rounded-[21px] shadow-[0px_2px_0px_0px_rgba(0,0,0,1.00)] border border-black flex justify-center pt-2">
+            <div className="flex flex-col w-[100%]">
+                <img className="w-[90%] h-[65%] rounded-2xl border border-black ml-auto mr-auto" src={"/freaky-bean.svg"} />
+                
+                <div className="ml-4 mt-2">
+                    <div className=" text-black text-[4.5vw] font-bold">{restaurant}</div>
+                    
+                    <div className="flex flex-row"> 
+                        <div className=" text-black text-[4vw] font-normal">{cuisine}</div>
+                        <span className= "ml-[30%] mr-[5px] self-center"> <img src="/star.svg"/></span> 
+                        <div className=" text-black text-[4vw] text-right font-bold " >{rating}<br/></div>
+                    </div>
+                </div>
+                
+                
+            </div>
+
+        {/** 
+        <div className="w-[167px] h-[167px] left-0 top-0 absolute bg-[#f7f6f4] rounded-[21px] shadow-[0px_2px_0px_0px_rgba(0,0,0,1.00)] border border-black" />
+        
+        
+        <div className="left-[16px] top-[122px] absolute text-black text-[15px] font-bold">{restaurant}</div>
+        
+
+        **/}
+    </div>
+    </div>
+    );
 }
