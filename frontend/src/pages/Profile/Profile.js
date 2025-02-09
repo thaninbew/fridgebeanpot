fec74c6ced6d1c037812131714ea5623773d30b9
olivial215 <li.ol@northeastern.edu>
2025-02-08 21:15:32 -0500

    achievements

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -1,24 +1,59 @@
 import Navbar from "../../components/Navbar";
 import styles from "./Profile.module.css";
+import Achievement from "./Achievement";
 
 export default function Profile() {
    return (
-    <div className="min-h-screen pb-24">
+    <div className="min-h-screen pb-24 font-Satoshi-Variable">
         <div className={styles.profileHeader}> 
             <div className={styles.wrapper}>
-                <img className={styles.profilePic} src="" alt="Profile"/>
-                <p className={styles.Name}> Name 
+                <img className={styles.profilePic} src=""/>
+                <p className="font-bold text-[7vw]"> Name 
                     <p className={styles.level}>Lv. 123</p>
                 </p>
             </div>
         </div>
 
+        <h2 className="text-2xl font-bold mb-4 p-10 pb-[4px]">Achievements</h2>
+        
+        {/** achievements container */}
+        <div className={styles.container}>
+            
+            <Achievement
+                img= ""
+                title="Culture Connoisseur"
+                sub="Try 10 different cuisines"/>
 
-        <div className="px-6">
-            <h2 className="text-2xl font-bold mb-4">Achievements</h2>
-            {/** achievements container */}
+            <Achievement
+                img= ""
+                title="Culture Connoisseur"
+                sub="Try 10 different cuisines"/>
 
-            {/** stats container */}
+            <Achievement
+                img= ""
+                title="Culture Connoisseur"
+                sub="Try 10 different cuisines"/>
+
+            <Achievement
+                img= ""
+                title="Culture Connoisseur"
+                sub="Try 10 different cuisines"/>
+
+            <Achievement
+                img= ""
+                title="Culture Connoisseur"
+                sub="Try 10 different cuisines"/>
+        </div>
+
+        
+        <h2 className="text-2xl font-bold mb-4 p-10 pb-[4px]">Stats</h2>
+
+        {/** stats container */}
+        <div className={styles.container}>
+            <div>
+            <p> <span className="font-bold pr-2">Total Restaurants Visited:</span>100</p>
+            <p> <span className="font-bold pr-2"> Most Frequently Visited:</span>Mamacita's</p>
+            </div>
         </div>
 
         <Navbar />
